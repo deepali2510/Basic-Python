7376ba0fe062cb6c418145b46d8b403869aaa126
deepali2510 <122172663+deepali2510@users.noreply.github.com>
2023-01-30 23:04:17 +0530

    Add files via upload
    Program to swap the values

---
+++ Basic_Python.py
@@ -0,0 +1,12 @@
+# Write a program that will convert celsius value to fahrenheit
+'''
+Celsius = float(input('Enter the temperature in celsius'))
+Fahrenheit = 1.8 * Celsius + 32
+print('Temperature in Fahrenhite',Fahrenheit)
+'''
+# User will input (2numbers).Write a program to swap the numbers
+a = 10
+b = 20
+a,b = b,a
+print(a)
+print(b)
